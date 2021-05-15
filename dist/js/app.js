e6e343ae6b53e0c0a73be22db0e69d55331d698d
renatech <33743002+renatech@users.noreply.github.com>
2021-05-15 04:24:02 +0100

    Update: Asset files updated

--- dist/js/app.js
+++ dist/js/app.js
@@ -17,4 +17,31 @@
     menuButton: document.querySelector('.sidebar .menu-button'),
     sideBar: document.querySelector('.sidebar'),
     sideBarDropdown: document.querySelectorAll('.sidebar .sidebar-dropdown')
-})
+});
+
+/**
+ * Point of sale code
+ */
+ (function(){
+    const posProduct = document.querySelectorAll('.pos-product');
+    const categoryTrigger = document.querySelector('#category_button');
+    const categorySidebar = document.querySelector('#category_sidebar');
+    const closeSidebarButton = document.querySelector('#close_category_sidebar');
+    const closeModalButton = document.querySelector('#close_modal')
+    const modal = document.querySelector('.modal-background')
+
+    categoryTrigger.addEventListener('click',(button)=>{
+        button.preventDefault()
+        categorySidebar.classList.remove('hidden')
+    })
+
+    closeSidebarButton.addEventListener('click',(button)=>{
+        button.preventDefault()
+        categorySidebar.classList.add('hidden')
+    })
+
+    closeModalButton.addEventListener('click',(event)=>{
+        modal.classList.add('hidden')
+    })
+
+})();
